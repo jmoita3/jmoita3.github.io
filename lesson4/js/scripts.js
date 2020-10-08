6b78549975a4b1e7947dee36e46d5a381acd85db
jmoita3 <moi16001@byui.edu>
2020-10-08 17:16:00 -0500

    worked on weather site

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -1,3 +1,11 @@
 function responsive() {
     document.getElementsByClassName("main-navigation-elements")[0].classList.toggle("expand");
 }
+
+window.addEventListener("load", (event)=>{
+    const lu = document.querySelector("#lastUpdated")
+    lu.textContent = document.lastModified;
+
+    const cry = document.querySelector("#copyYear");
+    cry.textContent = new Date().getFullYear();
+})
