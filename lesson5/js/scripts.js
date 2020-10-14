47077742bb7dd621c3f31385201849c31a0b8019
jmoita3 <moi16001@byui.edu>
2020-10-13 22:29:27 -0500

    added lesson 5

---
+++ lesson5/js/scripts.js
@@ -0,0 +1,17 @@
+function responsive() {
+    document.getElementsByClassName("main-navigation-elements")[0].classList.toggle("expand");
+}
+
+window.addEventListener("load", (event)=>{
+    const lu = document.querySelector("#lastUpdated")
+    lu.textContent = document.lastModified;
+
+    const cry = document.querySelector("#copyYear");
+    cry.textContent = new Date().getFullYear();
+})
+
+
+$(window).scroll(function(e){
+    parallax();
+  });
+  
