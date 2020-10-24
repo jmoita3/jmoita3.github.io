5efe509bc4ec42ce920ce816f06ebbd8f83d085e
jmoita3 <moi16001@byui.edu>
2020-10-24 12:43:14 -0500

    added wind chill

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,12 @@
+function windChill(){
+    var t = document.querySelector("#text1").innerHTML;
+    var s = document.querySelector("#text2").innerHTML;
+    var f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + 0.4275 * t * Math.pow(s,0.16);
+    if ( t <= 50 && s > 3){
+        document.getElementById("chills").innerHTML=f;
+    }
+    else{
+        document.getElementById("chills").innerHTML="N/A";
+    }
+            }
+    window.onload=windChill ; 
