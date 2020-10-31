5d7c970bcfd9ca1a41188986532f0906b19f192b
jmoita3 <moi16001@byui.edu>
2020-10-31 13:36:58 -0500

    committed

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -13,7 +13,7 @@ function preloadImage(img){
 
 const imgOptions = {
     threshold: 1,
-    rootMargin: "0px 0px -500px 0px"
+    rootMargin: "0px 0px 0px 0px"
 };
 
 const imgObserver = new IntersectionObserver((entries, 
