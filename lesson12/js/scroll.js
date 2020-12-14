2bcb7379729671c31a97f433eb78f8f195079853
jmoita3 <joaomoita3@hotmail.com>
2020-12-14 13:31:52 -0700

    more edits

--- lesson12/js/scroll.js
+++ lesson12/js/scroll.js
@@ -3,11 +3,19 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     var width = (window.innerWidth > 0) ? window.innerWidth : document.documentElement.clientWidth;
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 && width > 900) {
+      
     document.getElementById("header").style.fontSize = "12px";
     document.getElementById("main-navigation").style.paddingTop = "8px"; 
     document.getElementById("main-navigation").style.paddingBottom = "8px"; 
     document.getElementById("logo").style.minWidth = "100px"
     document.getElementById("logo").style.marginTop = "-5px"
+    let hero = document.getElementById("hero");
+    window.addEventListener("scroll", function(){
+        var value = window.scrollY;
+
+        hero.style.top = value * 0.5 + "px";
+
+    })
 
 } else {
   document.getElementById("header").style.fontSize = "14px";
@@ -15,6 +23,7 @@ function scrollFunction() {
   document.getElementById("main-navigation").style.paddingBottom = "15px"; 
   document.getElementById("logo").style.minWidth = "165px"
   document.getElementById("logo").style.marginTop = "0px"
+  
   }
 
 } 
